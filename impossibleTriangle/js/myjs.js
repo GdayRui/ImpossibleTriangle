51d3de5a3e731b4330fbf4fd979d7fcf54dd6613
GdayRui <lirui.jobs@gmail.com>
2019-09-27 00:37:57 +1000

    Initial commit

---
+++ impossibleTriangle/js/myjs.js
@@ -0,0 +1,20 @@
+
+jQuery(document).ready(function() {
+  jQuery('.left').addClass("hidden").viewportChecker({
+      classToAdd: 'visible animated bounceInLeft', // Class to add to the elements when they are visible
+      offset: 100
+     });
+});
+
+jQuery(document).ready(function() {
+  jQuery('.right').addClass("hidden").viewportChecker({
+      classToAdd: 'visible animated bounceInRight', // Class to add to the elements when they are visible
+      offset: 100
+     });
+});
+jQuery(document).ready(function() {
+  jQuery('.lg-icons').addClass("hidden").viewportChecker({
+      classToAdd: 'visible animated fadeInDown', // Class to add to the elements when they are visible
+      offset: 100
+     });
+});
