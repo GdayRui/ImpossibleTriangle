d431a4bfcb6f390e27624a6583bcb7f809a54d54
GdayRui <lirui.jobs@gmail.com>
2019-09-29 23:57:46 +1000

    Removed unused js file

--- js/myjs.js
+++ js/myjs.js
@@ -4,15 +4,12 @@ jQuery(document).ready(function() {
       classToAdd: 'visible animated bounceInLeft', // Class to add to the elements when they are visible
       offset: 100
      });
-});
 
-jQuery(document).ready(function() {
   jQuery('.right').addClass("hidden").viewportChecker({
       classToAdd: 'visible animated bounceInRight', // Class to add to the elements when they are visible
       offset: 100
      });
-});
-jQuery(document).ready(function() {
+
   jQuery('.lg-icons').addClass("hidden").viewportChecker({
       classToAdd: 'visible animated fadeInDown', // Class to add to the elements when they are visible
       offset: 100
